8bb6ea66bff7a5485b93ddf3ae2396918c775020
SolomonVP <Lum7023@yandex.ru>
2022-01-20 22:30:43 +0300

    66_1

--- src/main/java/com/victor/spring/mvc/MyController.java
+++ src/main/java/com/victor/spring/mvc/MyController.java
@@ -2,8 +2,11 @@ package com.victor.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -26,18 +29,13 @@ public class MyController {
 
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult) {
+//        System.out.println("surname length = " + emp.getSurname().length());
 
-        String name = emp.getName();
-        emp.setName("Mr. " + name);
-
-        String surname = emp.getSurname();
-        emp.setSurname(surname + "...");
-
-        int salary = emp.getSalary();
-        emp.setSalary(salary*11);
-
-        return "show-emp-details-view";
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-emp-details-view";
+        }
     }
-
 }
